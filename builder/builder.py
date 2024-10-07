b0c70d38840f68069fbb4095996e23e143fb43e8
Lucyy_ <72966048+LcyDev@users.noreply.github.com>
2024-10-07 15:34:57 -0300

    Implement ToggleFile

--- builder/builder.py
+++ builder/builder.py
@@ -1,4 +1,4 @@
-import os, subprocess, shutil
+import os, subprocess, shutil, re
 from sty import fg, bg
 
 from src.config import CFG, LoadConfig
@@ -55,8 +55,78 @@ def GetCMD_PyIns():
     cmd.append(pyins.cfg["script"])
     return cmd
 
+def ToggleFile(path: str, defaults: dict, state: bool, type_hint: bool):
+    if not os.path.exists(path):
+        return
+    hint = ': bool'*type_hint
+
+    with open(path, 'r') as f:
+        content = f.read()
+
+    for key, value in defaults.items():
+        pattern = rf'{key}{hint} = (True|False) # <>'
+        if value is None:
+            replacement = f'{key}{hint} = {state} # <>'
+        else:
+            replacement = f'{key}{hint} = {value} # <>'
+        content = re.sub(pattern, replacement, content)
+
+    with open(path, 'w') as f:
+        f.write(content)
+
+def ChangeDevMode(state: bool):
+    code_defaults = {
+        # Secret
+        'devMode': None,
+        'allowDev': None,
+        'allowMods': None,
+        # Cheats
+        'godMode': False,
+        'passThrough': False,
+        # Debug
+        'doClear': True,
+        'checkVersion': True,
+        'saveCFG': True,
+        'loadCFG': True,
+    }
+    CODE_PY = ''
+    ToggleFile(CODE_PY, code_defaults, state, type_hint=True)
+
+def ChangeCompileMode(state: bool):
+    master_defaults = {
+        'IS_COMPILED': None,
+    }
+    MASTER_PY = ''
+
+    ToggleFile(MASTER_PY, master_defaults, state, type_hint=False)
+
+
+def SetBuildMode(state):
+    with open("Master.py", 'r+') as f:
+        fileContent = f.read()
+        f.seek(0)
+        fileContent.replace()
+
+    rep = {
+        "doClear": True,
+        "allowDev": False,
+        "allowMods": False,
+        "checkVersion": True,
+        "saveCFG": True,
+        "loadCFG": True,
+    }
+
+    with open("_code.py", 'r+') as f:
+        fileContent = f.read()
+        f.seek(0)
+        for line in f.readlines():
+            line_split = line.lstrip().rstrip().split(': bool = ')
+            if len(line_split) == 1: continue
+            if val := rep.get(line_split[0]) is not None:
+                fileContent = fileContent.replace(f"{val}: bool = {line_split[1]}", f"{val}: bool = {state}")
+
 def Compile():
-    if not CFG.wood["dev-build"]:
+    if not CFG.wood.dev_build:
         ChangeDevMode(False)
     print()
     
