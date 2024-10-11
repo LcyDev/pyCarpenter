e9e0bbbcb1e43c161f6fa6abeb81d489a983b072
Lucyy_ <72966048+LcyDev@users.noreply.github.com>
2024-10-11 15:55:52 -0300

    Update config.py

--- builder/src/config.py
+++ builder/src/config.py
@@ -1,19 +1,21 @@
 import yaml
 
 class BuildConfig:
-    class wood:
+    class program:
         name: str
         version: str
         dev_build: bool
         beta_build: bool
 
     class build:
-        x64: bool
-        PreTest: bool
-        AutoClean: bool
-        AutoRun: bool
+        use_nuitka: bool
+        make_x64: bool
+        test_before: bool
+        auto_clean: bool
+        auto_run: bool
 
     class package:
+        output_dir: str
         folders: list[str]
         excluded: list[str]
         included: list[str]
@@ -27,15 +29,22 @@ class BuildConfig:
         ...
 
     class pyinstaller:
-        cfg: dict
-        paths: dict
-        options: dict
+        cfg: dict[str]
+        paths: dict[str]
+        options: dict[str]
         extra_data: list[str]
         extra_binary: list[str]
         import_paths: list[str]
         proyect_imports: list[str]
         hidden_imports: list[str]
         extras: list[str]
+
+    class obfuscator:
+        testing: bool
+        output_dir: str
+        retry: bool
+        flags: dict[str]
+        folders: dict[str, str]
 
 CFG = BuildConfig()
 
