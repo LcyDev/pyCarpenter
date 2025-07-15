96ff9a897a7691f232b54df8873b1ffd383f65a4
LcyDev <72966048+LcyDev@users.noreply.github.com>
2025-07-15 08:00:45 -0300

    Update utils.py

--- builder/src/utils.py
+++ builder/src/utils.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-from config import CFG, DEBUG, Work
+from config import CFG, DEBUG, WORK
 
 
 def success(msg: str):
@@ -13,25 +13,23 @@ def warn(msg: str):
 def error(e: Exception):
     ...
 
-def Set64Bits(state: bool):
-    Work.bits = '64' if state else '32'
-
 def is_onefile() -> bool:
     if CFG.build.use_nuitka:
         return ...
     else:
         return CFG.pyinstaller.options["onefile-mode"]
 
-def get_full_name():
-    name = f'{CFG.program["name"]}-{CFG.program["version"]}'
+def get_full_name() -> str:
+    name = f'{CFG.program.name}-{CFG.program.version}'
     if CFG.program.dev_build:
         name += '-[DEV]'
     if CFG.program.beta_build:
         name += '-[BETA]'
-    if Work.is_x64():
+    if WORK.is_x64():
         name += '_x64'
     else:
         name += '_x86'
+    return name
 
 def get_output_dir() -> Path:
     if CFG.build.use_nuitka:
